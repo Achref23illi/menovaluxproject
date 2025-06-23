2ec24b26d10bc3ff4049c48071ff6aee3e56e37a
Achref23illi <achrefarabi414@gmail.com>
2025-06-23 17:51:39 +0200

    theme fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <LocaleProvider>{children}</LocaleProvider>
         </ThemeProvider>
       </body>
