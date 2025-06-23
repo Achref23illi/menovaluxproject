9e53bb2dea64ebbcdd904826f683ff0dc90ec812
Achref23illi <achrefarabi414@gmail.com>
2025-06-23 18:01:19 +0200

    seo fixes and implimentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,55 @@ import { LocaleProvider } from '@/components/locale-provider'
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  metadataBase: new URL('https://www.menovalux.ca'), // TODO: update with production URL
+  title: {
+    default: 'Menovalux',
+    template: '%s | Menovalux',
+  },
+  description: 'Entreprise de menuiserie aluminium et PVC à Montréal, Québec.',
+  keywords: [
+    'menuiserie',
+    'aluminium',
+    'pvc',
+    'fenêtres',
+    'portes',
+    'Montréal',
+    'Québec',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: 'Menovalux',
+    description: 'Entreprise de menuiserie aluminium et PVC à Montréal, Québec.',
+    url: 'https://www.menovalux.ca',
+    siteName: 'Menovalux',
+    locale: 'fr_CA',
+    type: 'website',
+    images: [
+      {
+        url: '/images/logo.png',
+        width: 800,
+        height: 600,
+        alt: 'Menovalux logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Menovalux',
+    description: 'Entreprise de menuiserie aluminium et PVC à Montréal, Québec.',
+    images: ['/images/logo.png'],
+  },
+  alternates: {
+    canonical: '/',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+  generator: 'Next.js 15',
 }
 
 export default function RootLayout({
---
+++ next-sitemap.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  siteUrl: 'https://www.menovalux.ca', // TODO: update with production URL
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+} 
