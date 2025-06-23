d9ef23787d1e042c4cff107d5a694b02a88cada7
Achref23illi <achrefarabi414@gmail.com>
2025-06-23 18:08:47 +0200

    changes sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
-export default {
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
   siteUrl: 'https://www.menovalux.ca', // TODO: update with production URL
   generateRobotsTxt: true,
   sitemapSize: 7000,
